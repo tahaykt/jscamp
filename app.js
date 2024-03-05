125c79bcfc3230522f8ef5d591fd456e6e7e0bd4
Taha Yakıt <107004218+tahaykt@users.noreply.github.com>
2024-03-06 01:05:55 +0300

    5 mart

---
+++ app.js
@@ -0,0 +1,16 @@
+console.log("selam")
+
+let dolarBugun=9.30
+let DolarDun="çanakkale"
+
+let konutKredileri=["ihtiyaç kredisi","ev kredisi","araç kredisi"]
+console.log("<ul>")
+for (let i=0;i<konutKredileri.length;i++) {
+    console.log("<li>"+konutKredileri[i]+"</li>")
+    
+}
+
+
+console.log("/<ul>")
+
+
